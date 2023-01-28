b865fb5635dd1367dcc78ff5649da2e9e2072ff3
Taline Zeidan <taline.zeidan@hotmail.com>
2023-01-28 21:31:32 +0200

    added something

--- Me.java
+++ Me.java
@@ -2,6 +2,7 @@ public class Me {
 	public static void main(String[] args) {
 		System.out.println("Taline Zeidan");
 		System.out.println("Computer Engineering");
+		System.out.println("I am in my second year of study");
 	
 	}
 }
