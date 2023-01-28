d9cc6cf11b1930ee37f0f257dfd0b7e45bb5e9cf
Taline Zeidan <taline.zeidan@hotmail.com>
2023-01-28 19:22:29 +0200

    corrected errors

---
+++ Problems.java
@@ -0,0 +1,8 @@
+public class Problems{
+	public static void main (String [] args){
+		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("Hello, there are a lot");
+		System.out.println("of problems");
+
+	}
+}
