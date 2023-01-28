89c65fa4265627b885466e8b74a03162c068de4c
Taline Zeidan <taline.zeidan@hotmail.com>
2023-01-28 18:37:23 +0200

    My First Program is done

---
+++ MyFirstProgram.java
@@ -0,0 +1,9 @@
+public class MyFirstProgram 
+{
+	public static void main(String [] args) {
+		System.out.print("This is my ");
+		System.out.println("first Java program");
+		System.out.println("Programming is fun!");
+	}
+}
+
