e70d6fc23c927b041698f348420f53bea8a93a13
Taline Zeidan <taline.zeidan@hotmail.com>
2023-01-28 21:30:38 +0200

    removed something

--- Me.java
+++ Me.java
@@ -2,6 +2,6 @@ public class Me {
 	public static void main(String[] args) {
 		System.out.println("Taline Zeidan");
 		System.out.println("Computer Engineering");
-		System.out.println("I am in my second year of study");
+	
 	}
 }
