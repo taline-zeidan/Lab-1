8cbd13161be1a6fa2177cd5b9ea33d11e3d911fc
Taline Zeidan <taline.zeidan@hotmail.com>
2023-01-28 20:55:05 +0200

    Lab1 contains Poem.java now!

---
+++ Poem.java
@@ -0,0 +1,9 @@
+public class Poem{
+	public static void main (String[] args){
+		System.out.println("Roses are red");
+		System.out.println("Violets are blue");
+		System.out.println("Programming?!");
+		System.out.println("I have no clue!");
+	}
+
+}
